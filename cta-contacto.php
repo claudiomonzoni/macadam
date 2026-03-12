50f4d9d53171844ca8cb7c61b748536a6d018af9
claudiomonzoni <claudiomonzoni@hotmail.com>
2026-03-12 09:35:00 -0600

    ajustes

--- cta-contacto.php
+++ cta-contacto.php
@@ -1,5 +1,5 @@
 <section class="cta-contacto menosMarUp">
     <h2>Contactez-nous pour votre formation</h2>
     <p>Prêt à obtenir votre permis de conduire ? N'hésitez pas à nous contacter pour planifier vos leçons ou poser vos questions.</p>
-    <a href="contact.php" class="btn-cta">Demander un rendez-vous</a>
+    <a href="contact.php" class="btn-cta">Demander des informations</a>
 </section>
